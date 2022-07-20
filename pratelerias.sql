95c9602a97437d54469262d5803d9b9fba0e04ae
DeBaFig <denize.f.bassi@gmail.com>
2022-07-20 18:47:57 -0300

    COrrigido a necessidade de id_prateleira

--- pratelerias.sql
+++ pratelerias.sql
@@ -1,22 +1,18 @@
 BEGIN TRANSACTION;
-
-DROP TABLE IF EXISTS PRATELEIRAS; 
-
+DROP TABLE IF EXISTS PRATELEIRAS;
 CREATE TABLE PRATELEIRAS (
     id_prateleira INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_prateleira        TEXT     NOT NULL, 
-    localizacao_prateleira TEXT     NOT NULL
-
+    nome_prateleira TEXT NOT NULL,
+    localizacao_prateleira TEXT NOT NULL
 );
-
-COMMIT;
-
-INSERT INTO PRATELEIRAS (id_prateleira, nome_prateleira, localizacao_prateleira)
-    VALUES
-    ('Prateleira Salgados','Corredor 1'),
-    ('Prateleira Doces','Corredor 1'),
+INSERT INTO PRATELEIRAS (
+        nome_prateleira,
+        localizacao_prateleira
+    )
+VALUES ('Prateleira Salgados', 'Corredor 1'),
+    ('Prateleira Doces', 'Corredor 1'),
     ('Prateleira Frios', 'Corredor 2'),
-    ('Prateleira Congelados',	'Corredor 2'),
+    ('Prateleira Congelados', 'Corredor 2'),
     ('Prateleira Padaria', 'Corredor 3'),
     ('Prateleira Produtos de Limpeza', 'Corredor 4'),
     ('Prateleira Higiene', 'Corredor 4'),
@@ -24,8 +20,4 @@ INSERT INTO PRATELEIRAS (id_prateleira, nome_prateleira, localizacao_prateleira)
     ('Prateleira Bebidas', 'Corredor 6'),
     ('Prateleira Produtos Infantis', 'Corredor 7'),
     ('Prateleira Animais', 'Corredor 8');
-
-
-
-
-
+COMMIT;
